e3883d69613b5e1ecacc588f7d68cf9ebf5fe353
50UM3N <soumen2015.s.k@gmail.com>
2020-05-24 16:42:15 +0530

    minor style change

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ imp.addEventListener('input', function (e) {
     a = document.createElement('div');
     a.setAttribute('id', 'aslist');
     a.setAttribute('class', 'aslist');
-    document.getElementsByClassName('asinputbox')[0].parentNode.appendChild(a);
+    imp.parentNode.appendChild(a);
     fetch('/', {
         method: 'POST',
         headers: {
@@ -26,7 +26,12 @@ imp.addEventListener('input', function (e) {
         .then(data => {
             for (let i = 0; i < data.length; i++) {
                 b = document.createElement('div');
-                b.innerHTML = "<strong>" + data[i].name.slice(0, d.length) + "</strong>" + data[i].name.slice(d.length) + "<input type='hidden' value='" + data[i].name + "'>";
+                b.innerHTML = `
+                <svg  version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"> <g>  <g><path d="M256,0C153.755,0,70.573,83.182,70.573,185.426c0,126.888,165.939,313.167,173.004,321.035c6.636,7.391,18.222,7.378,24.846,0c7.065-7.868,173.004-194.147,173.004-321.035C441.425,83.182,358.244,0,256,0z M256,278.719 c-51.442,0-93.292-41.851-93.292-93.293S204.559,92.134,256,92.134s93.291,41.851,93.291,93.293S307.441,278.719,256,278.719z" /></g></g></svg>
+                <strong>${data[i].name.slice(0, d.length)}</strong>${data[i].name.slice(d.length)}
+                <span > ${data[i].country}</span>
+                <input type="hidden" value="${data[i].name}">
+            `
                 b.addEventListener('click', function () { imp.value = this.getElementsByTagName('input')[0].value; });
                 a.appendChild(b);
             }
